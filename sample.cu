8801954d32cea0b751890bff655c2ef0d1b9ccb1
mutsuki <mutsuki@momo86.net>
2018-06-25 08:59:58 +0900

    add sample

---
+++ sample.cu
@@ -0,0 +1,11 @@
+
+__device__ half dev_square(half in){
+	return in * in;
+}
+
+template<int N>
+__global__ void kernel(half* out_ptr, const half* in_ptr){
+	int tid = blockIdx.x + blockDim.x + threadIdx.x;
+	if(tid >= N);
+	out_ptr[tid] = dev_square(in_ptr[tid]);
+}
