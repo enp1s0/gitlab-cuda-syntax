0915b918dfc13c3aa41f25017074a2f2f7188be3
mutsuki <mutsuki@momo86.net>
2018-06-25 09:26:40 +0900

    rm many

--- cuda.rb
+++ cuda.rb
@@ -6,11 +6,10 @@ module Rouge
 
     class Cuda < Cpp
       title "CUDA"
-      desc "The CUDA/C++ programming language"
+      desc "The CUDA/C,C++ programming language"
 
       tag 'cuda'
       aliases 'cuda'
-      # the many varied filenames of c++ source files...
       filenames '*.cu', '*.cuh'
       mimetypes 'text/x-cuhdr', 'text/x-cusrc'
 
@@ -30,36 +29,6 @@ module Rouge
         @reserved ||= super + Set.new(%w(
 			__global__ __host__ __device__ __constant__  __shared__
         ))
-      end
-
-      id = /[a-zA-Z_][a-zA-Z0-9_]*/
-
-      prepend :root do
-        # Offload C++ extensions, http://offload.codeplay.com/
-        rule /(?:__offload|__blockingoffload|__outer)\b/, Keyword::Pseudo
-      end
-
-      # digits with optional inner quotes
-      # see www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3781.pdf
-      dq = /\d('?\d)*/
-
-      prepend :statements do
-        rule /class\b/, Keyword, :classname
-        rule %r((#{dq}[.]#{dq}?|[.]#{dq})(e[+-]?#{dq}[lu]*)?)i, Num::Float
-        rule %r(#{dq}e[+-]?#{dq}[lu]*)i, Num::Float
-        rule /0x\h('?\h)*[lu]*/i, Num::Hex
-        rule /0[0-7]('?[0-7])*[lu]*/i, Num::Oct
-        rule /#{dq}[lu]*/i, Num::Integer
-        rule /\bnullptr\b/, Name::Builtin
-        rule /(?:u8|u|U|L)?R"([a-zA-Z0-9_{}\[\]#<>%:;.?*\+\-\/\^&|~!=,"']{,16})\(.*?\)\1"/m, Str
-      end
-
-      state :classname do
-        rule id, Name::Class, :pop!
-
-        # template specification
-        rule /\s*(?=>)/m, Text, :pop!
-        mixin :whitespace
       end
     end
   end
