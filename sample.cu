f95d8a4f673e4f1594226b64ef8c5395b62a8262
mutsuki <mutsuki@momo86.net>
2018-07-22 19:18:37 +0900

    fix : add return

--- sample.cu
+++ sample.cu
@@ -6,6 +6,6 @@ __device__ half dev_square(half in){
 template<int N>
 __global__ void kernel(half* out_ptr, const half* in_ptr){
 	int tid = blockIdx.x + blockDim.x + threadIdx.x;
-	if(tid >= N);
+	if(tid >= N)return;
 	out_ptr[tid] = dev_square(in_ptr[tid]);
 }
